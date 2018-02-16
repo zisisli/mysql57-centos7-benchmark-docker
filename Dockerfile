fd8a648bb50a5be0d4c87fd91abcfa00b733df65
Zisis Lianas <zisis.lianas@consol.de>
2018-02-16 10:40:43 +0100

    Added test script for faster execution

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM centos/mysql-57-centos7
 USER root
 RUN  yum install -y mariadb-libs
 COPY files/sysbench-bin-0.4.12.14 /usr/bin/sysbench
+COPY files/00-mysql-bench.sh /scripts
+RUN  chmod 755 /scripts/00-mysql-bench.sh
 
 RUN  rm -rf /etc/my.cnf.d/*
 RUN  /usr/libexec/container-setup && rpm-file-permissions
---
+++ files/00-mysql-bench.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "drop table if existssbtest;" | mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -h 127.0.0.1 ${MYSQL_DATABASE}
+
+sysbench \
+  --test=oltp \
+  --oltp-table-size=20000 \
+  --mysql-user=${MYSQL_USER} \
+  --db-driver=mysql \
+  --mysql-password=${MYSQL_PASSWORD} \
+  --mysql-db=${MYSQL_DATABASE} \
+  --mysql-host=127.0.0.1 \
+  prepare
+
+sysbench \
+  --test=oltp \
+  --oltp-table-size=20000 \
+  --mysql-user=${MYSQL_USER} \
+  --db-driver=mysql \
+  --mysql-password=${MYSQL_PASSWORD} \
+  --mysql-db=${MYSQL_DATABASE} \
+  --mysql-host=127.0.0.1 \
+  run
+
